94a186ce8b644fbba382094f9e731a682925c8e9
Fredowisk <zcrazyhero@gmail.com>
2022-06-28 20:03:57 -0300

    refactor: move sut creation to a factory helper method

--- test/infra/cryptography/bcrypt-adapter.spec.ts
+++ test/infra/cryptography/bcrypt-adapter.spec.ts
@@ -6,19 +6,17 @@ jest.mock('bcrypt', () => ({
     return await Promise.resolve('hashed_password')
   }
 }))
+const salt = 12
+const sut = new BcryptAdapter(salt)
 
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('valid_password')
     expect(hashSpy).toHaveBeenCalledWith('valid_password', salt)
   })
 
   test('Should return hashed password on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
     const hash = await sut.encrypt('valid_password')
     expect(hash).toBe('hashed_password')
   })
