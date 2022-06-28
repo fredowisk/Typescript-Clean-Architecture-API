31215aab5f36de9a62e0972a7be3058f08babb44
Fredowisk <zcrazyhero@gmail.com>
2022-06-28 11:04:11 -0300

    test: ensure SignUpController return 200 if the account is created

--- test/presentation/controllers/signup.spec.ts
+++ test/presentation/controllers/signup.spec.ts
@@ -198,13 +198,32 @@ describe('SignUp Controller', () => {
       }
     }
 
-    const httpResponse = sut.handle(httpRequest)
+    sut.handle(httpRequest)
     expect(addAccountSpy).toHaveBeenCalledWith({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+  })
+
+  test('Should return 200 if account is created successfully', () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
     expect(httpResponse.statusCode).toBe(200)
-    expect(httpResponse.body.id).toBe('valid_id')
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
   })
 })
