0179946fdb084eabb3988b3dd1a6543f59014b99
Fredowisk <zcrazyhero@gmail.com>
2022-06-29 14:40:46 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express'
+
+const cors = (req: Request, res: Response, next: NextFunction): void => {
+  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-methods', '*')
+  res.set('access-control-allow-headers', '*')
+
+  next()
+}
+
+export default cors
