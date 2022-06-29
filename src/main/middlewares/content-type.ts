52e3319f7efa6cfdeb7e037c66a90428beed7b77
Fredowisk <zcrazyhero@gmail.com>
2022-06-29 14:49:07 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express'
+
+const contentType = (req: Request, res: Response, next: NextFunction): void => {
+  res.type('json')
+
+  next()
+}
+
+export default contentType
