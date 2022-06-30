2e25abfdef1db447b47b4495acb1ed1bf24750ff
Fredowisk <zcrazyhero@gmail.com>
2022-06-30 13:15:53 -0300

    test: ensure LogControllerDecorator calls controller with correct parameter

---
+++ tests/main/decorators/log.spec.ts
@@ -0,0 +1,41 @@
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols/'
+import { LogControllerDecorator } from '../../../src/main/decorators/log'
+
+describe('Log Controller Decorator', () => {
+  test('Should call controller handle', async () => {
+    class ControllerStub implements Controller {
+      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse: HttpResponse = {
+          statusCode: 200,
+          body: {
+            name: 'Fred'
+          }
+        }
+        return Promise.resolve(httpResponse)
+      };
+    }
+
+    const controllerStub = new ControllerStub()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const sut = new LogControllerDecorator(controllerStub)
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith({
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    })
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.name).toBe('Fred')
+  })
+})
