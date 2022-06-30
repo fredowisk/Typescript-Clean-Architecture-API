d47e231608579656cf1601229400cc02d97c4d7c
Fredowisk <zcrazyhero@gmail.com>
2022-06-30 20:52:48 -0300

    test: ensure ErrorMongoRepository create an error log on success

---
+++ tests/infra/database/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,29 @@
+import { MongoHelper } from '../../../../../src/infra/database/mongodb/helpers/mongo-helper'
+import { ErrorMongoRepository } from '../../../../../src/infra/database/mongodb/log-repository/log'
+
+describe('Log Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await Promise.all([MongoHelper.disconnect(), MongoHelper.clear('logs')])
+  })
+
+  const sut = new ErrorMongoRepository()
+
+  test('Should save the error stack and return nothing', async () => {
+    const fakeStack = 'any_stack'
+    const spyAccountRepository = jest.spyOn(sut, 'log')
+    await sut.log(fakeStack)
+
+    expect(spyAccountRepository).toHaveBeenCalledWith('any_stack')
+  })
+
+  test('Should return the quantity of logs in the collection', async () => {
+    const logsCollection = await MongoHelper.getCollection('logs')
+    const logCount = await logsCollection.countDocuments()
+
+    expect(logCount).toBe(1)
+  })
+})
