2f112d36272445538c996e4b355d6d40eef76933
Fredowisk <zcrazyhero@gmail.com>
2022-07-01 08:23:47 -0300

    refactor: remove duplicate code

--- tests/presentation/controllers/login/login.spec.ts
+++ tests/presentation/controllers/login/login.spec.ts
@@ -1,38 +1,37 @@
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { badRequest, ok } from '@/presentation/helpers/http-helper'
+import { HttpRequest } from '@/presentation/protocols'
 import { LoginController } from '../../../../src/presentation/controllers/login/login'
 
 describe('Login Controller', () => {
-  test('Should return 400 if no email is provided', async () => {
-    const sut = new LoginController()
-    const httpRequest = {
+  const sut = new LoginController()
+
+  const makeHttpRequest = (property?: string): HttpRequest => {
+    const httpRequest: HttpRequest = {
       body: {
+        email: 'any_email@mail.com',
         password: 'any_password'
       }
     }
+    if (property) httpRequest.body[property] = undefined
+
+    return httpRequest
+  }
+
+  test('Should return 400 if no email is provided', async () => {
+    const httpRequest = makeHttpRequest('email')
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
-    const httpRequest = {
-      body: {
-        email: 'any_email'
-      }
-    }
+    const httpRequest = makeHttpRequest('password')
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
 
   test('Should return 200 if controller is called with correct values', async () => {
-    const sut = new LoginController()
-    const httpRequest = {
-      body: {
-        email: 'any_email',
-        password: 'any_password'
-      }
-    }
+    const httpRequest = makeHttpRequest()
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(ok())
   })
