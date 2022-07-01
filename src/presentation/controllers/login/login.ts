3aad9b763e471b58201b485e71b26698980539eb
Fredowisk <zcrazyhero@gmail.com>
2022-07-01 08:37:28 -0300

    feat: ensure LoginController call EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,12 +1,36 @@
+import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
-import { badRequest, ok } from '@/presentation/helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols/'
+import {
+  badRequest,
+  ok,
+  serverError
+} from '@/presentation/helpers/http-helper'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from '@/presentation/protocols/'
+import { EmailValidator } from '../signup/signup-protocols'
 
 class LoginController implements Controller {
+  constructor (readonly emailValidator: EmailValidator) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) return Promise.resolve(badRequest(new MissingParamError('email')))
-    if (!httpRequest.body.password) return Promise.resolve(badRequest(new MissingParamError('password')))
-    return ok()
+    try {
+      if (!httpRequest.body.email) {
+        return Promise.resolve(badRequest(new MissingParamError('email')))
+      }
+      if (!httpRequest.body.password) {
+        return Promise.resolve(badRequest(new MissingParamError('password')))
+      }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) { return Promise.resolve(badRequest(new InvalidParamError('email'))) }
+
+      return ok()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
