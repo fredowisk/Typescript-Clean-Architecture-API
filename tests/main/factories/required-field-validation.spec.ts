4220cfa4ffeab327cb7cd330c7e7a27912f1e59c
Fredowisk <zcrazyhero@gmail.com>
2022-07-01 15:40:45 -0300

    refactor: remove code duplication

--- tests/main/factories/required-field-validation.spec.ts
+++ tests/main/factories/required-field-validation.spec.ts
@@ -2,17 +2,15 @@ import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { RequiredFieldValidation } from '@/presentation/helpers/validators/required-field-validation'
 
 describe('Required Field Validation', () => {
-  test('Should return MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('any_field')
+  const sut = new RequiredFieldValidation('field')
 
+  test('Should return MissingParamError if validation fails', () => {
     const error = sut.validate({ name: 'any_name' })
 
-    expect(error).toEqual(new MissingParamError('any_field'))
+    expect(error).toEqual(new MissingParamError('field'))
   })
 
   test('Should not return if validation succeeds', () => {
-    const sut = new RequiredFieldValidation('field')
-
     const error = sut.validate({ field: 'any_name' })
 
     expect(error).toBeFalsy()
