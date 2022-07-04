d1279e7a0f64abff2d3038dca3f35e67c86e3642
Fredowisk <zcrazyhero@gmail.com>
2022-07-04 14:50:37 -0300

    test: ensure UserAuthentication throw an Error if TokenGenerator fails

--- tests/data/usecases/authentication/user-authentication.spec.ts
+++ tests/data/usecases/authentication/user-authentication.spec.ts
@@ -127,4 +127,17 @@ describe('Authentication', () => {
 
     expect(generateSpy).toBeCalledWith(id)
   })
+
+  test('Should throw an Error TokenGenerator fails', async () => {
+    jest
+      .spyOn(tokenGeneratorStub, 'generate')
+      .mockReturnValueOnce(Promise.reject(new Error()))
+    const { email, password } = fakeAccount
+    const promise = sut.auth({
+      email,
+      password
+    })
+
+    await expect(promise).rejects.toThrow()
+  })
 })
