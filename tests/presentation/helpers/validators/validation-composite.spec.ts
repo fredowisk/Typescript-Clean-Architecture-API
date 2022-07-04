9222598aa4cb99f1571595de5461b16dd52e030a
Fredowisk <zcrazyhero@gmail.com>
2022-07-04 09:53:20 -0300

    test: ensure ValidationComposite returns stop the validation and return the first error

--- tests/presentation/helpers/validators/validation-composite.spec.ts
+++ tests/presentation/helpers/validators/validation-composite.spec.ts
@@ -9,16 +9,31 @@ describe('Validation Composite', () => {
     }
   }
 
-  const validationStub = new ValidationStub()
+  const validationStubs = [new ValidationStub(), new ValidationStub()]
   test('Should return the same error of validation if validation fails', () => {
     jest
-      .spyOn(validationStub, 'validate')
+      .spyOn(validationStubs[1], 'validate')
       .mockReturnValueOnce(new InvalidParamError('field'))
-    const sut = new ValidationComposite([validationStub])
+    const sut = new ValidationComposite(validationStubs)
     const error = sut.validate({
       field: 'any_value'
     })
 
     expect(error).toEqual(new InvalidParamError('field'))
+  })
+
+  test('Should stop validation and return the first validation error', () => {
+    jest
+      .spyOn(validationStubs[0], 'validate')
+      .mockReturnValueOnce(new Error())
+    jest
+      .spyOn(validationStubs[1], 'validate')
+      .mockReturnValueOnce(new InvalidParamError('field'))
+    const sut = new ValidationComposite(validationStubs)
+    const error = sut.validate({
+      field: 'any_value'
+    })
+
+    expect(error).toEqual(new Error())
   })
 })
