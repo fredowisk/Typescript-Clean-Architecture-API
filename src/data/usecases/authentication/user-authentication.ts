0dfbc1f513817e40890d29782df63de7dc8286fa
Fredowisk <zcrazyhero@gmail.com>
2022-07-04 14:48:04 -0300

    feat: ensure UserAuthentication calls TokenGenerator with correct id

--- src/data/usecases/authentication/user-authentication.ts
+++ src/data/usecases/authentication/user-authentication.ts
@@ -1,12 +1,14 @@
 import { Authentication } from '@/application/usecases/authentication/authentication'
 import { AuthenticationModel } from '@/application/usecases/authentication/authentication-model'
 import { HashComparer } from '@/data/protocols/criptography/hash-comparer'
+import { TokenGenerator } from '@/data/protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository'
 
 class UserAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -23,7 +25,9 @@ class UserAuthentication implements Authentication {
 
     if (!isEqual) return null
 
-    return 'access_token'
+    const accessToken = await this.tokenGenerator.generate(user.id)
+
+    return accessToken
   }
 }
 
