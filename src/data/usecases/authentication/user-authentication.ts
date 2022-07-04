284d19a7da06a7a8f738990813b0b2c11754540b
Fredowisk <zcrazyhero@gmail.com>
2022-07-04 13:31:23 -0300

    feat: ensure UserAuthentication calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/authentication/user-authentication.ts
+++ src/data/usecases/authentication/user-authentication.ts
@@ -1,8 +1,19 @@
 import { Authentication } from '@/application/usecases/authentication/authentication'
 import { AuthenticationModel } from '@/application/usecases/authentication/authentication-model'
+import { LoadAccountByEmailRepository } from '@/data/protocols/load-account-by-email-repository'
 
 class UserAuthentication implements Authentication {
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  ) {}
+
   async auth (authentication: AuthenticationModel): Promise<string> {
+    const user = await this.loadAccountByEmailRepository.load(
+      authentication.email
+    )
+
+    if (!user) return null
+
     return 'access_token'
   }
 }
