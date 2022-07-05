9c486d92510c9f07941d63a6c396a9ed470c37dd
Fredowisk <zcrazyhero@gmail.com>
2022-07-05 11:32:13 -0300

    test: add login route

--- tests/main/routes/account-routes.test.ts
+++ tests/main/routes/account-routes.test.ts
@@ -1,6 +1,8 @@
 import { MongoHelper } from '@/infra/database/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '@/main/config/app'
+import { hash } from 'bcrypt'
+import env from '../config/env'
 
 describe('Account Routes', () => {
   beforeAll(async () => {
@@ -15,18 +17,26 @@ describe('Account Routes', () => {
     await MongoHelper.clear('accounts')
   })
 
+  const fakeAccount = {
+    name: 'Fred',
+    email: 'fred@gmail.com',
+    password: '123',
+    passwordConfirmation: '123'
+  }
+
   describe('POST /signup', () => {
     test('Should return 200 if signup succeeds', async () => {
-      await request(app)
-        .post('/api/signup')
-        .send({ name: 'Fred', email: 'fred@gmail.com', password: '123', passwordConfirmation: '123' })
-        .expect(200)
+      await request(app).post('/api/signup').send(fakeAccount).expect(200)
     })
   })
 
   describe('POST /login', () => {
-    test('should ', () => {
-
+    test('Should return 200 on login ', async () => {
+      const password = await hash('123', env.salt)
+      const accountCollection = await MongoHelper.getCollection('accounts')
+      const { name, email } = fakeAccount
+      await accountCollection.insertOne({ name, email, password })
+      await request(app).post('/api/login').send({ email, password })
     })
   })
 })
