37b4cea6734601b9c7b3237337b5e301c8a180a7
Fredowisk <zcrazyhero@gmail.com>
2022-07-05 11:18:30 -0300

    feat: add LoginFactory

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,15 +1,30 @@
 import { UserAuthentication } from '@/data/usecases/authentication/user-authentication'
 import { JwtAdapter } from '@/infra/cryptography/jwt-adapter/jwt-adapter'
+import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
 import { LoginController } from '@/presentation/controllers/login/login-controller'
 import { Controller } from '@/presentation/protocols/controller'
 import { loginValidation } from './login-validation-factory'
+import { ErrorMongoRepository } from '@/infra/database/mongodb/log-repository/log-mongo-repository'
+import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { AccountMongoRepository } from '@/infra/database/mongodb/account-repository/account-mongo-repository'
+import env from '@/main/config/env'
 
 export const makeLoginController = (): Controller => {
-  const loadAccountByEmailRepository = Account
-  const hashComparer = new HashComparer()
-  const encrypterGenerator = new JwtAdapter('secret')
-  const updateAccessTokenRepository = new UpdateAccessTokenRepository()
-  const authentication = new UserAuthentication(loadAccountByEmailRepository, hashComparer, encrypterGenerator, updateAccessTokenRepository)
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(env.salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const authentication = new UserAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository
+  )
 
-  return new LoginController(authentication, loginValidation())
+  const loginController = new LoginController(
+    authentication,
+    loginValidation()
+  )
+  const logMongoRepository = new ErrorMongoRepository()
+
+  return new LogControllerDecorator(loginController, logMongoRepository)
 }
--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -6,17 +6,14 @@ import { Controller } from '@/presentation/protocols'
 import SignUpController from '@/presentation/controllers/signup/signup-controller'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { signUpValidation } from './signup-validation-factory'
+import env from '@/main/config/env'
 
 export const makeSignUpController = (): Controller => {
-  const salt = 12
-  const hasher = new BcryptAdapter(salt)
+  const hasher = new BcryptAdapter(env.salt)
   const addAccountRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(hasher, addAccountRepository)
 
-  const signUpController = new SignUpController(
-    addAccount,
-    signUpValidation()
-  )
+  const signUpController = new SignUpController(addAccount, signUpValidation())
   const logErrorRepository = new ErrorMongoRepository()
   return new LogControllerDecorator(signUpController, logErrorRepository)
 }
