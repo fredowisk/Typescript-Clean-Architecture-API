763a9f57b1a14568bae490409e562cebd4ec3e66
Fredowisk <zcrazyhero@gmail.com>
2022-07-05 09:28:49 -0300

    test: ensure AccountMongoRepository returns an account if loadByEmail succeeds

--- tests/infra/database/mongodb/account-repository/account.spec.ts
+++ tests/infra/database/mongodb/account-repository/account.spec.ts
@@ -14,19 +14,30 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.clear('accounts')
   })
 
-  test('Should return nothing on success', async () => {
-    const sut = new AccountMongoRepository()
-    const spyAccountRepository = jest.spyOn(sut, 'add')
-    await sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
-    })
+  const sut = new AccountMongoRepository()
 
-    expect(spyAccountRepository).toHaveBeenCalledWith({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
-    })
+  const fakeAccount = {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+
+  test('Should return nothing if add succeeds', async () => {
+    const spyAccountRepository = jest.spyOn(sut, 'add')
+    const account = await sut.add(fakeAccount)
+
+    expect(spyAccountRepository).toHaveBeenCalledWith(fakeAccount)
+    expect(account).toBeFalsy()
+  })
+
+  test('Should return an account if loadByEmail succeeds', async () => {
+    const spyAccountRepository = jest.spyOn(sut, 'add')
+    await sut.add(fakeAccount)
+    const { email } = fakeAccount
+    const account = await sut.loadByEmail(email)
+
+    expect(spyAccountRepository).toHaveBeenCalledWith(email)
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
   })
 })
