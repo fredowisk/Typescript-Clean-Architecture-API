28c438fac2d43e18f40d40ab4f43ace0b4d9e3fe
Fredowisk <zcrazyhero@gmail.com>
2022-07-06 16:20:57 -0300

    feat: ensure AddSurveyController return 500 if  AddSurveyUseCase throw an Error

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,19 +1,35 @@
-import { badRequest, ok } from '@/presentation/helpers/http/http-helper'
-import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
+import {
+  badRequest,
+  ok,
+  serverError
+} from '@/presentation/helpers/http/http-helper'
+import {
+  AddSurvey,
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation
+} from './add-survey-controller-protocols'
 
 class AddSurveyController implements Controller {
-  constructor (private readonly validation: Validation,
-    private readonly addSurvey: AddSurvey) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) return badRequest(error)
 
-    const { question, answers } = httpRequest.body
+      const { question, answers } = httpRequest.body
 
-    await this.addSurvey.add({ question, answers })
+      await this.addSurvey.add({ question, answers })
 
-    return Promise.resolve(ok())
+      return Promise.resolve(ok())
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
