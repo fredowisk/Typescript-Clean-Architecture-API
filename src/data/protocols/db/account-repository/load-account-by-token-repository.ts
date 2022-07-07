4323f324cf69154a4ccc12af2f71ed7847e56809
Fredowisk <zcrazyhero@gmail.com>
2022-07-07 15:32:37 -0300

    Revert "test: ensure AuthMiddleware returns 403 if no headers are found"
    This reverts commit 23f7f426eae2cee7e05c9afce68dfbe227fa1ff6.

--- src/data/protocols/db/account-repository/load-account-by-token-repository.ts
+++
@@ -1,7 +0,0 @@
-import { AccountModel } from '@/domain/models/account'
-
-interface LoadAccountByTokenRepository {
-  load: (token: string) => Promise<AccountModel>
-}
-
-export { LoadAccountByTokenRepository }
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,23 +1,10 @@
-import { LoadAccountByTokenRepository } from '@/data/protocols/db/account-repository/load-account-by-token-repository'
 import { AccessDeniedError } from '../errors'
-import { forbidden, ok } from '../helpers/http/http-helper'
+import { forbidden } from '../helpers/http/http-helper'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 class AuthMiddleware implements Middleware {
-  constructor (
-    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
-  ) {}
-
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers['x-access-token']
-
-    if (!accessToken) {
-      return forbidden(new AccessDeniedError())
-    }
-
-    const account = await this.loadAccountByTokenRepository.load(accessToken)
-
-    return ok(account)
+    return Promise.resolve(forbidden(new AccessDeniedError()))
   }
 }
 
