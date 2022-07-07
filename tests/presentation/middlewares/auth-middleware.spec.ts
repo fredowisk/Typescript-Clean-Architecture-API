2beea0c0c5ebda4567022524007395023748905e
Fredowisk <zcrazyhero@gmail.com>
2022-07-07 15:53:40 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- tests/presentation/middlewares/auth-middleware.spec.ts
+++ tests/presentation/middlewares/auth-middleware.spec.ts
@@ -2,11 +2,27 @@ import { HttpRequest } from '@/presentation/protocols'
 import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { AccessDeniedError } from '@/presentation/errors/access-denied-error'
 import { AuthMiddleware } from '@/presentation/middlewares/auth-middleware'
+import { AccountModel } from '@/domain/models/account'
+import { LoadAccountByToken } from 'application/usecases/load-account/load-account-by-token'
 
 describe('Auth Middleware', () => {
-  test('Should return 403 if there is no x-access-token in headers', async () => {
-    const sut = new AuthMiddleware()
+  const fakeAccount: AccountModel = {
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email',
+    password: 'hashed_password'
+  }
 
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+      return Promise.resolve(fakeAccount)
+    }
+  }
+
+  const loadAccountByTokenStub = new LoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+
+  test('Should return 403 if there is no x-access-token in headers', async () => {
     const httpRequest: HttpRequest = {
       headers: {}
     }
@@ -14,5 +30,21 @@ describe('Auth Middleware', () => {
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const loadAccountSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+
+    const httpRequest: HttpRequest = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(loadAccountSpy).toHaveBeenCalledWith(
+      httpRequest.headers['x-access-token']
+    )
   })
 })
