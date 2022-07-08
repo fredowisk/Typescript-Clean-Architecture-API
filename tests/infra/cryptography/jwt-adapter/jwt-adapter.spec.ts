2b9e1fa003467c85f2d321379dfd54a1e777fd3b
Fredowisk <zcrazyhero@gmail.com>
2022-07-08 09:41:11 -0300

    test: ensure JwtAdapter call verify with correct values

--- tests/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ tests/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,25 +1,32 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from '@/infra/cryptography/jwt-adapter/jwt-adapter'
+import env from '@/main/config/env'
 
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return Promise.resolve('token')
+  },
+
+  async verify (): Promise<string> {
+    return Promise.resolve('any_value')
   }
 }))
 
 describe('Jwt Adapter', () => {
-  const sut = new JwtAdapter('secret')
+  const sut = new JwtAdapter(env.jwtSecret)
+  const id = 'any_id'
+  const token = 'any_token'
 
   describe('sign()', () => {
     test('Should call sign with correct values', async () => {
       const signSpy = jest.spyOn(jwt, 'sign')
-      await sut.encrypt('any_id')
+      await sut.encrypt(id)
 
-      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+      expect(signSpy).toHaveBeenCalledWith({ id }, env.jwtSecret)
     })
 
     test('Should return a token if sign succeeds', async () => {
-      const accessToken = await sut.encrypt('any_id')
+      const accessToken = await sut.encrypt(id)
 
       expect(accessToken).toBe('token')
     })
@@ -28,9 +35,18 @@ describe('Jwt Adapter', () => {
       jest.spyOn(jwt, 'sign').mockImplementation(() => {
         throw new Error()
       })
-      const promise = sut.encrypt('any_id')
+      const promise = sut.encrypt(id)
 
       await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('verify()', () => {
+    test('Should call verify with correct values ', async () => {
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt(token)
+
+      expect(verifySpy).toHaveBeenCalledWith(token, env.jwtSecret)
     })
   })
 })
