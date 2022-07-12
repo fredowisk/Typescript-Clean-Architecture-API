601bb7725cfbeddc7b640205fc9cc8abfea73349
Fredowisk <zcrazyhero@gmail.com>
2022-07-12 10:46:12 -0300

    feat: ensure load surveys route returns 403 if no access token is provided

---
+++ src/main/factories/survey/load-survey/load-survey-factory.ts
@@ -0,0 +1,18 @@
+import { DbLoadSurveys } from '@/data/usecases/load-surveys/db-load-surveys'
+import { ErrorMongoRepository } from '@/infra/database/mongodb/log-repository/log-mongo-repository'
+import { SurveyMongoRepository } from '@/infra/database/mongodb/survey-repository/survey-mongo-repository'
+import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
+import { LoadSurveysController } from '@/presentation/controllers/survey/load-survey/load-surveys-controller'
+import { Controller } from '@/presentation/protocols/controller'
+
+const makeLoadSurveysController = (): Controller => {
+  const loadSurveysRepository = new SurveyMongoRepository()
+  const loadSurveys = new DbLoadSurveys(loadSurveysRepository)
+  const loadSurveysController = new LoadSurveysController(loadSurveys)
+  return new LogControllerDecorator(
+    loadSurveysController,
+    new ErrorMongoRepository()
+  )
+}
+
+export { makeLoadSurveysController }
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,9 +3,12 @@ import { adaptMiddleware } from '../adapters/express/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 import { makeAddSurveyController } from '../factories/survey/add-survey/add-survey-factory'
+import { makeLoadSurveysController } from '../factories/survey/load-survey/load-survey-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleware())
 
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
