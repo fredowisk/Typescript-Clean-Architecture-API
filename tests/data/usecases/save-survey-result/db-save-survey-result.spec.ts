86cb32987e2fbeae3c91f6657198bc3e118ec565
Fredowisk <zcrazyhero@gmail.com>
2022-07-13 11:06:16 -0300

    test: ensure DbSaveSurveyResult return a survey result on success

--- tests/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ tests/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -4,27 +4,36 @@ import { SaveSurveyResultModel } from '@/application/usecases/save-survey-result
 import { SurveyResultModel } from '@/domain/models/survey-result'
 
 describe('Db Save Survey Result', () => {
-  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
-    async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-      return Promise.resolve(null)
-    }
-  }
-
-  const saveSurveyResultRepositoryStub = new SaveSurveyResultRepositoryStub()
-  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
-
   const fakeResult = {
+    id: 'any_id',
     surveyId: 'survey_id',
     accountId: 'account_id',
     answer: 'any_answer',
     date: new Date()
   }
 
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return Promise.resolve(fakeResult)
+    }
+  }
+
+  const saveSurveyResultRepositoryStub = new SaveSurveyResultRepositoryStub()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+
+  const { id, ...fakeDataResult } = fakeResult
+
   test('Should call SaveSurveyResultRepository with correct values', async () => {
     const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
 
-    await sut.save(fakeResult)
+    await sut.save(fakeDataResult)
 
-    expect(saveSpy).toHaveBeenCalledWith(fakeResult)
+    expect(saveSpy).toHaveBeenCalledWith(fakeDataResult)
+  })
+
+  test('Should return survey result on success', async () => {
+    const surveyResult = await sut.save(fakeDataResult)
+
+    expect(surveyResult).toEqual(fakeResult)
   })
 })
