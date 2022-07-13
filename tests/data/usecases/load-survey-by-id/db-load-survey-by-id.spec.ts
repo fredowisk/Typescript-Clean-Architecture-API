d74fa0ae62c214dc5a75711103359e57bbd2b183
Fredowisk <zcrazyhero@gmail.com>
2022-07-13 10:24:34 -0300

    test: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with a correct id

---
+++ tests/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,49 @@
+import { DbLoadSurveyById } from '@/data/usecases/load-survey-by-id/db-load-survey-by-id'
+import { SurveyModel } from '@/domain/models/survey'
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+
+describe('Db LoadSurvey By Id', () => {
+  const fakeSurvey: SurveyModel = {
+    id: 'any_id',
+    question: 'any_question',
+    answer: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      }
+    ],
+    date: new Date()
+  }
+
+  class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
+    async loadById (id: string): Promise<SurveyModel> {
+      return Promise.resolve(fakeSurvey)
+    }
+  }
+  const loadSurveyByIdRepositoryStub = new LoadSurveyByIdRepositoryStub()
+  const sut = new DbLoadSurveyById(loadSurveyByIdRepositoryStub)
+
+  test('Should call LoadSurveyByIdRepository with a correct id', async () => {
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+
+    await sut.loadById(fakeSurvey.id)
+
+    expect(loadByIdSpy).toHaveBeenCalledWith(fakeSurvey.id)
+  })
+
+  test('Should return a survey on success', async () => {
+    const survey = await sut.loadById(fakeSurvey.id)
+
+    expect(survey).toEqual(fakeSurvey)
+  })
+
+  test('Should throw an Error if LoadSurveyByIdRepository throws an Error', async () => {
+    jest
+      .spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+      .mockRejectedValueOnce(new Error())
+
+    const promise = sut.loadById(fakeSurvey.id)
+
+    await expect(promise).rejects.toThrow()
+  })
+})
