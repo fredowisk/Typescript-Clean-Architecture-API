fbaddbf2f210754dc72fbb170daeb831b97bcd2f
Fredowisk <zcrazyhero@gmail.com>
2022-07-14 09:38:12 -0300

    test: ensure SaveSurveyResultController returns 500 if SaveSurveyResult throw an Error

--- tests/presentation/controllers/survey/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ tests/presentation/controllers/survey/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -117,4 +117,12 @@ describe('Save Survey Result Controller', () => {
       date: new Date()
     })
   })
+
+  test('Should return 500 if SaveSurveyResult throw an Error', async () => {
+    jest.spyOn(saveSurveyResultStub, 'save').mockRejectedValueOnce(new Error())
+
+    const httpResponse = await sut.handle(fakeRequest)
+
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
