9de42c5390872b55105d73ab319e57e830facb5d
Fredowisk <zcrazyhero@gmail.com>
2022-07-19 19:57:29 -0300

    test: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

---
+++ tests/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,31 @@
+import { LoadSurveyResultRepository } from "@/data/protocols/db/survey-result-repository/load-survey-result-repository";
+import { mockSurveyResultModel } from "@/tests/utils/index";
+import { DbLoadSurveyResult } from "@/data/usecases/survey/load-survey-result/db-load-survey-result";
+import { SurveyResultModel } from "@/domain/models/survey/survey-result";
+
+describe("DbLoadSurveyResult Use Case", () => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    loadBySurveyId(
+      surveyId: string,
+      accountId: string
+    ): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel());
+    }
+  }
+
+  const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub();
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub);
+
+  test("Should call LoadSurveyResultRepository with correct values", async () => {
+    const loadBySurveyId = jest.spyOn(
+      loadSurveyResultRepositoryStub,
+      "loadBySurveyId"
+    );
+
+    await sut.load("any_survey_id", "any_account_id");
+    expect(loadBySurveyId).toHaveBeenCalledWith(
+      "any_survey_id",
+      "any_account_id"
+    );
+  });
+});
