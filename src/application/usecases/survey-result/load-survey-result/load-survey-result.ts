35ff38f32c39e497a2a3728ae19613ed613c2270
Fredowisk <zcrazyhero@gmail.com>
2022-07-19 19:57:16 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

--- src/application/usecases/survey-result/load-survey-result/load-survey-result.ts
+++ src/application/usecases/survey-result/load-survey-result/load-survey-result.ts
@@ -1,7 +1,7 @@
 import { SurveyResultModel } from "@/domain/models/survey/survey-result";
 
 interface LoadSurveyResult {
-  load(surveyId: string): Promise<SurveyResultModel>;
+  load(surveyId: string, accountId: string): Promise<SurveyResultModel>;
 }
 
 export { LoadSurveyResult };
---
+++ src/data/usecases/survey/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,16 @@
+import { LoadSurveyResult } from "@/application/usecases/survey-result/load-survey-result/load-survey-result";
+import { LoadSurveyResultRepository } from "@/data/protocols/db/survey-result-repository/load-survey-result-repository";
+import { SurveyResultModel } from "@/domain/models/survey/survey-result";
+
+class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor(
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+  ) {}
+
+  async load(surveyId: string, accountId: string): Promise<SurveyResultModel> {
+    await this.loadSurveyResultRepository.loadBySurveyId(surveyId, accountId);
+    return Promise.resolve(null);
+  }
+}
+
+export { DbLoadSurveyResult };
