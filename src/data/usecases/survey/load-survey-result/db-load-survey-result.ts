5702ba2baf155456a1e7930644cdefb18b8fac57
Fredowisk <zcrazyhero@gmail.com>
2022-07-20 09:25:45 -0300

    feat: ensure DBLoadSurveyResult call LoadSurveyByIdRepository if returns null

--- src/data/usecases/survey/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey/load-survey-result/db-load-survey-result.ts
@@ -1,18 +1,25 @@
 import { LoadSurveyResult } from "@/application/usecases/survey-result/load-survey-result/load-survey-result";
 import { LoadSurveyResultRepository } from "@/data/protocols/db/survey-result-repository/load-survey-result-repository";
 import { SurveyResultModel } from "@/domain/models/survey/survey-result";
+import { LoadSurveyByIdRepository } from "../load-survey-by-id/db-load-survey-by-id-protocols";
 
 class DbLoadSurveyResult implements LoadSurveyResult {
   constructor(
-    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
   ) {}
 
   async load(surveyId: string, accountId: string): Promise<SurveyResultModel> {
-    const survey = await this.loadSurveyResultRepository.loadBySurveyId(
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(
       surveyId,
       accountId
     );
-    return survey;
+
+    if(!surveyResult) {
+      await this.loadSurveyByIdRepository.loadById(surveyId);
+    }
+    
+    return surveyResult;
   }
 }
 
