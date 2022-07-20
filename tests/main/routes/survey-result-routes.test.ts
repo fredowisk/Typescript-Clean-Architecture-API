06d82a96790c76a486f76b043bc74bf814ff4359
Fredowisk <zcrazyhero@gmail.com>
2022-07-20 14:08:57 -0300

    test: ensure load survey route returns 403 if no accessToken is provided

--- tests/main/routes/survey-result-routes.test.ts
+++ tests/main/routes/survey-result-routes.test.ts
@@ -1,48 +1,54 @@
-import request from 'supertest'
-import app from '@/main/config/app'
-import { MongoHelper } from '@/infra/database/mongodb/helpers/mongo-helper'
-import { Collection } from 'mongodb'
-import { mockAddSurveyParams, mockAccessToken } from '@/tests/utils'
+import request from "supertest";
+import app from "@/main/config/app";
+import { MongoHelper } from "@/infra/database/mongodb/helpers/mongo-helper";
+import { Collection } from "mongodb";
+import { mockAddSurveyParams, mockAccessToken } from "@/tests/utils";
 
-describe('Survey Result Routes', () => {
-  let accountCollection: Collection
-  let surveyCollection: Collection
-  let accessToken: string
+describe("Survey Result Routes", () => {
+  let accountCollection: Collection;
+  let surveyCollection: Collection;
+  let accessToken: string;
 
   beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL)
+    await MongoHelper.connect(process.env.MONGO_URL);
     await Promise.all([
-      MongoHelper.clear('accounts'),
-      MongoHelper.clear('surveys'),
-      MongoHelper.clear('surveyResults'),
-      (accountCollection = await MongoHelper.getCollection('accounts')),
-      (surveyCollection = await MongoHelper.getCollection('surveys')),
-      (accessToken = await mockAccessToken(accountCollection))
-    ])
-  })
+      MongoHelper.clear("accounts"),
+      MongoHelper.clear("surveys"),
+      MongoHelper.clear("surveyResults"),
+      (accountCollection = await MongoHelper.getCollection("accounts")),
+      (surveyCollection = await MongoHelper.getCollection("surveys")),
+      (accessToken = await mockAccessToken(accountCollection)),
+    ]);
+  });
 
-  const fakeSurvey = mockAddSurveyParams()
+  const fakeSurvey = mockAddSurveyParams();
 
-  describe('PUT /surveys/:surveyId/results', () => {
-    test('Should return 403 when call SaveSurveyResult without accessToken', async () => {
+  describe("PUT /surveys/:surveyId/results", () => {
+    test("Should return 403 when call SaveSurveyResult without accessToken", async () => {
       await request(app)
-        .put('/api/surveys/any_id/results')
+        .put("/api/surveys/any_id/results")
         .send({
-          answer: 'any_answer'
+          answer: "any_answer",
         })
-        .expect(403)
-    })
+        .expect(403);
+    });
 
-    test('Should return 200 on save survey result with accessToken', async () => {
-      const { insertedId } = await surveyCollection.insertOne(fakeSurvey)
+    test("Should return 200 on save survey result with accessToken", async () => {
+      const { insertedId } = await surveyCollection.insertOne(fakeSurvey);
 
       await request(app)
         .put(`/api/surveys/${insertedId}/results`)
-        .set('x-access-token', accessToken)
+        .set("x-access-token", accessToken)
         .send({
-          answer: 'any_answer'
+          answer: "any_answer",
         })
-        .expect(200)
-    })
-  })
-})
+        .expect(200);
+    });
+  });
+
+  describe("GET /surveys/:surveyId/results", () => {
+    test("Should return 403 on load survey result without accessToken", async () => {
+      await request(app).get("/api/surveys/any_id/results").expect(403);
+    });
+  });
+});
