9d425e5577c9d32c7c25512122435be7df11fcea
Fredowisk <zcrazyhero@gmail.com>
2022-07-20 10:08:57 -0300

    test: ensure DbLoadSurveyResult return surveyResultModel with all answers with count and percent equal 0 if Repository returns null

--- tests/data/usecases/load-survey-result/db-load-survey-result.spec.ts
+++ tests/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -1,5 +1,5 @@
 import { LoadSurveyResultRepository } from "@/data/protocols/db/survey-result-repository/load-survey-result-repository";
-import { mockSurveyModel, mockSurveyResultModel } from "@/tests/utils/index";
+import {  mockSurveyResultModel } from "@/tests/utils/index";
 import { DbLoadSurveyResult } from "@/data/usecases/survey/load-survey-result/db-load-survey-result";
 import { SurveyResultModel } from "@/domain/models/survey/survey-result";
 import { LoadSurveyByIdRepository } from "data/protocols/db/survey-repository/load-survey-by-id-repository";
@@ -17,7 +17,21 @@ describe("DbLoadSurveyResult Use Case", () => {
 
   class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
     loadById(id: string): Promise<SurveyModel> {
-      return Promise.resolve(mockSurveyModel());
+      return Promise.resolve({
+        id: "any_id",
+        question: "any_question",
+        answers: [
+          {
+            image: "any_image",
+            answer: "any_answer",
+          },
+          {
+            answer: "other_answer",
+            image: "any_image",
+          },
+        ],
+        date: new Date(),
+      });
     }
   }
 
@@ -59,6 +73,20 @@ describe("DbLoadSurveyResult Use Case", () => {
     await sut.load("any_survey_id", "any_account_id");
 
     expect(loadByIdSpy).toHaveBeenCalledWith("any_survey_id");
+  });
+
+  test("Should return surveyResultModel with all answers with count and percent equal 0 if LoadSurveyResultRepository returns null", async () => {
+    jest
+      .spyOn(loadSurveyResultRepositoryStub, "loadBySurveyId")
+      .mockResolvedValueOnce(null);
+
+    const surveyResult = await sut.load("any_survey_id", "any_account_id");
+
+    expect(surveyResult.answers).toHaveLength(2);
+    expect(surveyResult.answers[0].count).toBe(0);
+    expect(surveyResult.answers[0].percent).toBe(0);
+    expect(surveyResult.answers[1].count).toBe(0);
+    expect(surveyResult.answers[1].percent).toBe(0);
   });
 
   test("Should return a surveyResultModel on success", async () => {
